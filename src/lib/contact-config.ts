0c4cac0610a3f5cf8249a37d509c009ae4afaf3e
RCSowjanya <rcsowjanya@example.com>
2026-06-02 18:28:16 +0530

    changes

--- src/lib/contact-config.ts
+++ src/lib/contact-config.ts
@@ -1,4 +1,4 @@
 export const CONTACT_EMAIL = process.env.CONTACT_EMAIL ?? "rcsowjanya@gmail.com";
 export const CONTACT_NOTIFICATION_EMAIL =
-  process.env.CONTACT_NOTIFICATION_EMAIL ?? "rcsowjanya72@gmail.com";
+  process.env.CONTACT_NOTIFICATION_EMAIL ?? "rcsowjanya@gmail.com";
 export const CONTACT_LOCATION = "New York, USA";
